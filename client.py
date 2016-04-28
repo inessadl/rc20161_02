78dada059df6eff81c9950f8af8d17d02c711c4c
Deh-alba <andre90012002@gmail.com>
2016-04-27 21:18:43 -0300

    cliente enviando arquivos para um server. Server recebendo por threads

--- client.py
+++ client.py
@@ -35,23 +35,44 @@ class Client:
     def clientThread(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((TCP_IP, TCP_PORT))
-        with open('received_file', 'wb') as f:
-            print 'file opened'
-            while True:
-                # print('receiving data...')
-                data = s.recv(BUFFER_SIZE)
-                print('data=%s', (data))
-                if not data:
-                    f.close()
-                    print 'file close()'
-                    break
-                # write data to a file
-                f.write(data)
+        filename = 'testing.js'
+        # filename = 'mytext.txt'     # file received
+        f = open(filename, 'rb')
+        print('sending data...')
+        while True:
+            l = f.read(BUFFER_SIZE)
+            while (l):
+                s.send(l)
+                # print('Sent ',repr(l))
+                l = f.read(BUFFER_SIZE)
+            if not l:
+                f.close()
+                print('Successfully put the file')
+                s.close()
+                print('connection closed')
+                break
 
-        print('Successfully get the file')
-        s.close()
-        print('connection closed')
-
+print "cliente"
 cliente = Client()
-
 cliente.clientThread()
+
+'''    def clientThread(self):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((TCP_IP, TCP_PORT))
+            with open('received_file', 'wb') as f:
+                print 'file opened'
+                while True:
+                    # print('receiving data...')
+                    data = s.recv(BUFFER_SIZE)
+                    print('data=%s', (data))
+                    if not data:
+                        f.close()
+                        print 'file close()'
+                        break
+                    # write data to a file
+                    f.write(data)
+
+            print('Successfully get the file')
+            s.close()
+            print('connection closed')
+'''
--- server.py
+++ server.py
@@ -53,7 +53,7 @@ class ClientThread(Thread):
         self.sock = sock
         print " New thread started for "+ip+":"+str(port)
 
-    def run(self):
+    '''def run(self):
         filename = 'testing.js'
         # filename = 'mytext.txt'     # file received
         f = open(filename, 'rb')
@@ -67,18 +67,39 @@ class ClientThread(Thread):
                 f.close()
                 self.sock.close()
                 break
+    '''
+    def run(self):
+        with open('received_file', 'wb') as f:
+            print 'file opened'
+            while True:
+                # print('receiving data...')
+                data = self.sock.recv(BUFFER_SIZE)
+                print('data=%s', (data))
+                if not data:
+                    f.close()
+                    print 'file close()'
+                    break
+                # write data to a file
+                f.write(data)
+
+        print('Successfully get the file')
+        self.sock.close()
+        print('connection closed')
+        print "\n"
+
 
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 tcpsock.bind((TCP_IP, TCP_PORT))
 threads = []
 
 while True:
     tcpsock.listen(5)
     print "Waiting for incoming connections..."
-    (conn, (ip, port)) = tcpsock.accept()
-    print 'Got connection from ', (ip, port)
-    newthread = ClientThread(ip, port, conn)
+    # (conn, (ip, port)) = tcpsock.accept()
+    conn, addr = tcpsock.accept()
+    print 'Got connection from ', (TCP_IP, TCP_PORT)
+    newthread = ClientThread(TCP_IP, addr, conn)
     newthread.start()
     threads.append(newthread)
 
